8c110eb5ed202c7bf89f0c831193df67d924f07e
erikvw <ew2789@gmail.com>
2018-08-02 23:13:24 -0500

    add authentication backend to limit login by site

---
+++ edc_auth/backends.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+UserModel = get_user_model()
+
+
+class ModelBackendWithSite(ModelBackend):
+
+    """An authentication backend to only allow a login
+    associated with the current SITE_ID.
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        user = super().authenticate(request, username=username, password=password)
+        if user:
+            sites = [obj.id for obj in user.userprofile.sites.all()]
+            if (user.is_superuser or settings.SITE_ID in sites):
+                return user
+        return None
