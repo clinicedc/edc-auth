57851c722d1d356c2950fd75c5ef2d63955ff242
erikvw <ew2789@gmail.com>
2018-11-11 16:08:04 -0600

    add UserProfile admin page with custom changelist

--- edc_auth/admin.py
+++ edc_auth/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-
+from django.utils.safestring import mark_safe
 
 from .models import UserProfile
 
@@ -13,12 +13,40 @@ class UserProfileInline(admin.StackedInline):
 
     filter_horizontal = (
         'email_notifications',
-        'sms_notifications', )
+        'sms_notifications',
+        'sites')
 
 
 class UserAdmin(BaseUserAdmin):
     inlines = (UserProfileInline, )
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    filter_horizontal = (
+        'email_notifications',
+        'sms_notifications',
+        'sites')
+
+    list_display = ('user', 'user_sites',
+                    'user_email_notifications',
+                    'user_sms_notifications',
+                    'mobile')
+
+    def user_sites(self, obj=None):
+        return mark_safe('<BR>'.join([
+            o.name for o in obj.sites.all().order_by('name')]))
+
+    def user_email_notifications(self, obj=None):
+        return mark_safe('<BR>'.join([
+            o.display_name for o in obj.email_notifications.all().order_by(
+                'display_name')]))
+
+    def user_sms_notifications(self, obj=None):
+        return mark_safe('<BR>'.join([
+            o.display_name for o in obj.sms_notifications.all().order_by(
+                'display_name')]))
+
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
