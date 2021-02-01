3123f44a94ccff1af6d20f9f8d46538e6d3e73bd
erikvw <ew2789@gmail.com>
2021-01-31 21:05:24 -0600

    README, setup.py

--- edc_auth/views/login_view.py
+++ edc_auth/views/login_view.py
@@ -11,11 +11,13 @@ class LoginView(BaseLoginView):
 
     def get_context_data(self, **kwargs):
         """Tests cookies."""
+        context = super().get_context_data(**kwargs)
+        context.update(**self.extra_context)
         self.request.session.set_test_cookie()
         if not self.request.session.test_cookie_worked():
             messages.add_message(self.request, messages.ERROR, "Please enable cookies.")
         self.request.session.delete_test_cookie()
-        return super().get_context_data(**kwargs)
+        return context
 
     @property
     def extra_context(self):
