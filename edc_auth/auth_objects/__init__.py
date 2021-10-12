b2c6ded316f96b53e6e8d819f5bd8b4882df9eea
erikvw <ew2789@gmail.com>
2021-10-11 23:47:45 -0500

    move out pharmacy auths

--- edc_auth/auth_objects/__init__.py
+++ edc_auth/auth_objects/__init__.py
@@ -5,9 +5,7 @@ from .codenames import (
     celery_manager,
     clinic,
     clinic_super,
-    dispensing,
     everyone,
-    pharmacy,
 )
 from .default_group_names import (
     ACCOUNT_MANAGER,
@@ -16,9 +14,7 @@ from .default_group_names import (
     CELERY_MANAGER,
     CLINIC,
     CLINIC_SUPER,
-    DISPENSING,
     EVERYONE,
-    PHARMACY,
     PII,
     PII_VIEW,
 )
@@ -31,8 +27,6 @@ from .default_role_names import (
     CLINICIAN_SUPER_ROLE,
     CUSTOM_ROLE,
     NURSE_ROLE,
-    PHARMACIST_ROLE,
-    SITE_PHARMACIST_ROLE,
     STAFF_ROLE,
 )
 from .default_roles import default_roles
