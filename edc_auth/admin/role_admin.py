b55d9e525899205fa940a31f8b7d0c38a93732e1
erikvw <ew2789@gmail.com>
2022-08-10 17:21:00 -0500

    modeladmin return tuples

--- edc_auth/admin/role_admin.py
+++ edc_auth/admin/role_admin.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from django.contrib import admin
 
 from ..models import Role
@@ -8,10 +10,10 @@ class RoleAdmin(admin.ModelAdmin):
 
     fieldsets = ((None, ({"fields": ("display_name", "name", "display_index", "groups")})),)
 
-    list_display = ["display_name", "name", "display_index"]
+    list_display: Tuple[str, ...] = ("display_name", "name", "display_index")
 
-    filter_horizontal = ("groups",)
+    filter_horizontal: Tuple[str, ...] = ("groups",)
 
-    search_fields = ("display_name", "name", "groups__name")
+    search_fields: Tuple[str, ...] = ("display_name", "name", "groups__name")
 
-    ordering = ("display_index", "display_name")
+    ordering: Tuple[str, ...] = ("display_index", "display_name")
